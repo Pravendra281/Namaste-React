bbf38df7b28126a89eec481cb47624f1d5594b87
Pravendra <pravendrasingh281@gmail.com>
2023-04-20 08:06:01 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
+const title = <h1>Namaste React  episode 3</h1>;
+
+// const HeadingComponent =()=><h1>React is beast </h1>;
+
+const HeadingComponent2 = ()=> (
+    <div id="container">
+    {title}
+     <h1>React is beast </h1>
+    </div>
+)
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent2/>);
