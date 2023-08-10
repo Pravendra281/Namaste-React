d397b7d584a2f2cbc7db77f7829040ccc22cb309
Pravendra <pravendrasingh281@gmail.com>
2023-08-10 20:45:29 +0530

    updated Restaurantcards

--- src/component/Body.js
+++ src/component/Body.js
@@ -29,9 +29,9 @@ const Body=()=>{
         console.log(json)
         // setListOfResturants(json?.data?.cards[2]?.data?.data?.cards);
         // New Api
-        setListOfResturants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setListOfResturants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         // New Api
-        setFilteredRestaurants( json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants( json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
     const onlineStatus=useOnlineStatus();
